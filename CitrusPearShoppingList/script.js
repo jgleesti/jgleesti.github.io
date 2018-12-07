51ea89fd6f96db3f744b827e7b3399b8d1865268
Jordan Lee <jordan.lee@myfastpc.com>
2018-12-06 18:04:57 -0700

    Added background image, new title, and the associated recipe for the ingredient.

--- CitrusPearShoppingList/script.js
+++ CitrusPearShoppingList/script.js
@@ -1,28 +1,28 @@
 var data = {
     title: "Citrus Pear",
     items: [
-        {name: "Brown rice, quinoa, or Asian noodle", checked: false},
-        {name: "Whole wheat pita bread", checked: false},
-        {name: "Plain low fat Greek yogurt", checked: false},
-        {name: "Lemon juice", checked: false},
-        {name: "Cucumber", checked: false},
-        {name: "Dried dill", checked: false},
-        {name: "Prepared tzatziki sauce (if desired)", checked: false},
-        {name: "Whole wheat tortillas", checked: false},
-        {name: "Lettuce", checked: false},
-        {name: "Shredded cheese", checked: false},
-        {name: "Desired taco toppings", checked: false},
-        {name: "Parmesan cheese if desired", checked: false},
-        {name: "Brown rice", checked: false},
-        {name: "Green onions", checked: false},
-        {name: "Sesame Seeds", checked: false},
-        {name: "Noodle of choice", checked: false},
-        {name: "Whole wheat tortillas", checked: false},
-        {name: "Additional salad/taco toppings", checked: false},
-        {name: "Brown rice", checked: false},
-        {name: "Tortillas", checked: false},
-        {name: "Chicken broth for tortilla soup", checked: false},
-        {name: "Fresh 'taco style' toppings", checked: false}
+        {name: "Brown rice, quinoa, or Asian noodle", checked: false, recipe: "Cashew Chicken"},
+        {name: "Whole wheat pita bread", checked: false, recipe: "Chicken Gyros"},
+        {name: "Plain low fat Greek yogurt", checked: false, recipe: "Chicken Gyros"},
+        {name: "Lemon juice", checked: false, recipe: "Chicken Gyros"},
+        {name: "Cucumber", checked: false, recipe: "Chicken Gyros"},
+        {name: "Dried dill", checked: false, recipe: "Chicken Gyros"},
+        {name: "Prepared tzatziki sauce (if desired)", checked: false, recipe: "Chicken Gyros"},
+        {name: "Whole wheat tortillas", checked: false, recipe: "Chicken Lime Tacos"},
+        {name: "Lettuce", checked: false, recipe: "Chicken Lime Tacos"},
+        {name: "Shredded cheese", checked: false, recipe: "Chicken Lime Tacos"},
+        {name: "Desired taco toppings", checked: false, recipe: "Chicken Lime Tacos"},
+        {name: "Parmesan cheese if desired", checked: false, recipe: "Italian Sausage & Tortellini Soup"},
+        {name: "Brown rice", checked: false, recipe: "Korean Beef"},
+        {name: "Green onions", checked: false, recipe: "Korean Beef"},
+        {name: "Sesame Seeds", checked: false, recipe: "Korean Beef"},
+        {name: "Noodle of choice", checked: false, recipe: "Pesto Tossed Lasagna"},
+        {name: "Whole wheat tortillas", checked: false, recipe: "Sweet Pork"},
+        {name: "Additional salad/taco toppings", checked: false, recipe: "Sweet Pork"},
+        {name: "Brown rice", checked: false, recipe: "Tex-Mex Chicken"},
+        {name: "Tortillas", checked: false, recipe: "Tex-Mex Chicken"},
+        {name: "Chicken broth for tortilla soup", checked: false, recipe: "Tex-Mex Chicken"},
+        {name: 'Fresh "taco style" toppings', checked: false, recipe: "Tex-Mex Chicken"}
     ],
     newItem: ''
 };
@@ -40,7 +40,7 @@ var vm = new Vue({
         addItem: function() {
             var item = this.newItem;
 
-            this.items.unshift({name: item, checked: false});
+            this.items.unshift({name: item, checked: false, recipe:'Custom'});
             this.newItem = '';
         },
         checkboxToggle: function(item) {
